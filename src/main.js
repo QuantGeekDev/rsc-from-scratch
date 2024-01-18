071294f83cd4084c485b02a98a95fecae6590104
Alex Andru <alex007d@gmail.com>
2024-01-18 03:18:38 +0100

    Add basic router with /, /croqueta, and /botifarra

--- src/main.js
+++ src/main.js
@@ -3,18 +3,21 @@ import { createServer } from "http";
 import Recipe from "./Recipe.js";
 import { readFile } from "fs/promises";
 
-const croquetaRecipe = new Recipe("./recipes/croqueta.txt");
 const port = process.env.PORT ?? 8080;
 
 createServer(async (req, res) => {
+  console.log(req.url);
+  if (isFaviconRequest(req)) {
+    console.log("Favicon has been requested");
+  }
+
   if (isCssRequest(req)) {
     const css = await readFile("./src/styles.css", "utf-8");
     sendCSS(res, css);
     return;
   }
-
-  const html = await croquetaRecipe.generateHtml();
-  sendHTML(res, html);
+  await router(req, res);
+  return;
 }).listen(port);
 
 console.log(`Listening on http://localhost:${port}`);
@@ -31,5 +34,24 @@ const sendCSS = (res, css) => {
   res.end(css);
 };
 
-const isCssRequest = (req) =>
-  req.headers.accept.includes("text/css") ? true : false;
+const isCssRequest = (req) => req.headers.accept.includes("text/css");
+
+const isFaviconRequest = (req) => (req.url === "/favicon.ico" ? true : false);
+
+const router = async (req, res) => {
+  const { url } = req;
+  switch (url) {
+    case "/botifarra":
+      const botifarraRecipe = new Recipe("./recipes/botifarra.txt");
+      const botifarraHtml = await botifarraRecipe.generateHtml();
+      sendHTML(res, botifarraHtml);
+      break;
+    case "croqueta":
+    case "/":
+      const croquetaRecipe = await new Recipe("./recipes/croqueta.txt");
+      const croquetaHtml = await croquetaRecipe.generateHtml();
+      await sendHTML(res, croquetaHtml);
+    default:
+      return;
+  }
+};
