4f12b888af4aae503b686c81e914b91ac330822e
Alex Andru <alex007d@gmail.com>
2024-01-18 13:13:24 +0100

    Extract server logic into it's own class

---
+++ src/Server.js
@@ -0,0 +1,49 @@
+import { readFile } from "fs/promises";
+
+class Server {
+  port;
+  router;
+  constructor(port, router) {
+    this.port = port;
+    this.router = router;
+  }
+
+  start = async (req, res) => {
+    if (this.constructor.isFaviconRequest(req)) {
+      const favicon = await readFile("./favicon.ico");
+      this.sendFavicon(res, favicon);
+    }
+
+    if (this.constructor.isCssRequest(req)) {
+      const css = await readFile("./src/styles.css", "utf-8");
+      this.sendCSS(res, css);
+      return;
+    }
+    await this.router(req, res);
+  };
+
+  sendHTML = (res, html) => {
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(html);
+  };
+
+  sendCSS = (res, css) => {
+    res.setHeader("Content-Type", "text/css");
+    res.writeHead(200);
+    res.end(css);
+  };
+
+  sendFavicon = (res, favicon) => {
+    res.setHeader("Content-Type", "image/x-icon");
+    res.writeHead(200);
+    res.end(favicon);
+  };
+
+  static isCssRequest = (req) => req.headers.accept.includes("text/css");
+
+  static isFaviconRequest = (req) =>
+    req.url === "/favicon.ico" ? true : false;
+}
+
+export default Server;
